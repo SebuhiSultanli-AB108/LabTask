2a274f255370f394f18490b9e62013e94121ddee
Sabuhi <sabuhies-ap118@code.edu.az>
2024-10-27 19:56:02 +0400

    aded the car lab task

---
+++ Car/Models/Car.cs
@@ -0,0 +1,38 @@
+﻿namespace CarsAndStuff.Models;
+
+class Car
+{
+    public int Id { get; set; }
+    public int Year { get; set; }
+    public int EngineCode { get; set; }
+    public string Brand { get; set; }
+    public string Model { get; set; }
+
+    public static bool operator <(Car car1, Car car2)
+    {
+        return car1.Year < car2.Year;
+    }
+    public static bool operator >(Car car1, Car car2)
+    {
+        return car1.Year > car2.Year;
+    }
+}
+
+static class Helper
+{
+    public static void OrderByYear(this Car[] cars)
+    {
+        for (int i = 0; i < cars.Length; i++)
+        {
+            for (int j = 0; j < cars.Length; j++)
+            {
+                if (j != cars.Length - 1 && cars[j] > cars[j + 1])
+                {
+                    Car newcar = cars[j];
+                    cars[j] = cars[j + 1];
+                    cars[j + 1] = newcar;
+                }
+            }
+        }
+    }
+}
---
+++ Car/Program.cs
@@ -0,0 +1,56 @@
+﻿using CarsAndStuff.Models;
+
+namespace CarsAndStuff;
+
+internal class Program
+{
+    static void Main()
+    {
+        Car myCar1 = new Car()
+        {
+            Id = 1,
+            Brand = "dsa",
+            Model = "sss",
+            Year = 2000,
+            EngineCode = 1234565
+        };
+        Car myCar2 = new Car()
+        {
+            Id = 2,
+            Brand = "mer",
+            Model = "sss",
+            Year = 2020,
+            EngineCode = 1234565
+        };
+        Car myCar3 = new Car()
+        {
+            Id = 3,
+            Brand = "mehryer",
+            Model = "sss",
+            Year = 1980,
+            EngineCode = 1234565
+        };
+        Car myCar4 = new Car()
+        {
+            Id = 4,
+            Brand = "gjrt",
+            Model = "sss",
+            Year = 2024,
+            EngineCode = 1234565
+        };
+        Car[] cars = { myCar1, myCar2, myCar3, myCar4 };
+        cars.OrderByYear();
+        foreach (Car car in cars)
+        {
+            Console.WriteLine($"""
+                               =================================
+                               Id: {car.Id}
+                               Brand: {car.Brand}
+                               Model: {car.Model}
+                               Year: {car.Year}
+                               Engine Code: {car.EngineCode}
+                               """);
+        }
+        Console.WriteLine("=================================");
+    }
+}
